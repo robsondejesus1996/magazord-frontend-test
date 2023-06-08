5b0514276ddd7cd31bf3527dfaa60f46dfafffa2
robsondejesus1996 <robsondejesus1996@hotmail.com>
2023-06-08 12:11:15 -0300

    teste de estilo e configuração sass

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles/global.scss'
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello World</h1>
     </div>
   );
 }
